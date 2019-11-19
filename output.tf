8a2e7d9122e055b771c1b581a4f761db43014c9c
Thomas Bell <tbell@dramafever.com>
2019-11-18 21:23:03 -0500

    tf12

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "loggshipper_lambda_arn" {
   description = "ARN of logshipper lambda function"
-  value       = "${aws_lambda_function.s3_logshipper.arn}"
+  value       = aws_lambda_function.s3_logshipper.arn
 }
 
 output "logshipper_role_arn" {
   description = "ARN of logshipper role"
-  value       = "${aws_iam_role.s3_logshipper.arn}"
+  value       = aws_iam_role.s3_logshipper.arn
 }
